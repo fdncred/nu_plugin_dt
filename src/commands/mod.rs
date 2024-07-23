f90173004837859b3bd27bb21d3b7f9f9265fd4a
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2024-07-23 15:45:51 -0500

    add plumbing for 2 new commands

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,5 +1,9 @@
 // Command modules should be added here
 mod add;
+mod now;
+mod utcnow;
 
 // Command structs should be exported here
 pub use add::Add;
+pub use now::Now;
+pub use utcnow::UtcNow;
---
+++ src/commands/utcnow.rs
@@ -0,0 +1,71 @@
+use crate::DtPlugin;
+use nu_plugin::{EngineInterface, EvaluatedCall, SimplePluginCommand};
+use nu_protocol::{Category, Example, LabeledError, Signature, SyntaxShape, Value};
+
+pub struct UtcNow;
+
+impl SimplePluginCommand for UtcNow {
+    type Plugin = DtPlugin;
+
+    fn name(&self) -> &str {
+        "utcnow"
+    }
+
+    fn signature(&self) -> Signature {
+        Signature::build(self.name())
+            .required(
+                "name",
+                SyntaxShape::String,
+                "(FIXME) A demo parameter - your name",
+            )
+            .switch("shout", "(FIXME) Yell it instead", None)
+            .category(Category::Experimental)
+    }
+
+    fn usage(&self) -> &str {
+        "(FIXME) help text for add"
+    }
+
+    fn examples(&self) -> Vec<Example> {
+        vec![
+            Example {
+                example: "add Ellie",
+                description: "Say hello to Ellie",
+                result: Some(Value::test_string("Hello, Ellie. How are you today?")),
+            },
+            Example {
+                example: "add --shout Ellie",
+                description: "Shout hello to Ellie",
+                result: Some(Value::test_string("HELLO, ELLIE. HOW ARE YOU TODAY?")),
+            },
+        ]
+    }
+
+    fn run(
+        &self,
+        _plugin: &DtPlugin,
+        _engine: &EngineInterface,
+        call: &EvaluatedCall,
+        _input: &Value,
+    ) -> Result<Value, LabeledError> {
+        let name: String = call.req(0)?;
+        let mut greeting = format!("Hello, {name}. How are you today?");
+        if call.has_flag("shout")? {
+            greeting = greeting.to_uppercase();
+        }
+        Ok(Value::string(greeting, call.head))
+    }
+}
+
+#[test]
+fn test_examples() -> Result<(), nu_protocol::ShellError> {
+    use nu_plugin_test_support::PluginTest;
+
+    // This will automatically run the examples specified in your command and compare their actual
+    // output against what was specified in the example.
+    //
+    // We recommend you add this test to any other commands you create, or remove it if the examples
+    // can't be tested this way.
+
+    PluginTest::new("dt", DtPlugin.into())?.test_command_examples(&UtcNow)
+}
